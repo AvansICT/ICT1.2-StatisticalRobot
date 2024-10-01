86dc27d17678ab366b13eed199fec6becaae45fc
idorf79 <f.steffers@avans.nl>
2024-10-01 12:32:12 +0200

    update to make it function

--- StatisticalRobot-Lib/StatisticalRobot-Lib/Actuators.cs
+++ StatisticalRobot-Lib/StatisticalRobot-Lib/Actuators.cs
@@ -1,70 +1,68 @@
 ﻿using Avans.StatisticalRobot;
 using System.Device.I2c;
 
-namespace Avans.StatisticalRobot;
-
-public interface IActuator
+namespace Avans.StatisticalRobot
 {
-}
-
-public interface IDisplay : IActuator
-{
-    public void SetText(string text);
-
-    public void ClearDisplay();
-}
-
-public  class I2CDisplay : IDisplay
-{
-    private byte _i2c_address = 0x00;
-    private I2cDevice _device;
-
-    public I2CDisplay(byte i2c_address)
+    public interface IActuator
     {
-        _i2c_address = i2c_address;
-        _device = Robot.CreateI2cDevice(_i2c_address);
     }
 
-    private void WriteCommandToDisplay(byte cmd)
+    public interface IDisplay : IActuator
     {
-        _device.WriteByteRegister(0x80, cmd);
+        public void SetText(string text);
+
+        public void ClearDisplay();
     }
 
-    public void ClearDisplay()
+    public class I2CDisplay : IDisplay
     {
-        WriteCommandToDisplay(0x01);
-    }
+        private byte _i2c_address = 0x00;
+        private I2cDevice _device;
 
-    public void SetText(string text)
-    {
-        if (_i2c_address == 0x00)
+        public I2CDisplay(byte i2c_address)
         {
-            throw new Exception("Display address not set");
+            _i2c_address = i2c_address;
+            _device = Robot.CreateI2cDevice(_i2c_address);
         }
 
-        ClearDisplay();
-        Thread.Sleep(50);
-        WriteCommandToDisplay(0x08 | 0x04); // display on, no cursor
-        WriteCommandToDisplay(0x28); // 2 lines
-        Thread.Sleep(50);
-        int count = 0;
-        int row = 0;
-
-        foreach (char c in text)
+        private void WriteCommandToDisplay(byte cmd)
         {
-            if (c == '\n' || count == 16)
+            _device.WriteByteRegister(0x80, cmd);
+        }
+
+        public void ClearDisplay()
+        {
+            WriteCommandToDisplay(0x01);
+        }
+
+        public void SetText(string text)
+        {
+            if (_i2c_address == 0x00)
             {
-                count = 0;
-                row++;
-                if (row == 2) break;
-                WriteCommandToDisplay(0xc0);
-                if (c == '\n') continue;
+                throw new Exception("Display address not set");
             }
-            count++;
-            _device.WriteByteRegister(0x40, (byte)c);
+
+            ClearDisplay();
+            Thread.Sleep(50);
+            WriteCommandToDisplay(0x08 | 0x04); // display on, no cursor
+            WriteCommandToDisplay(0x28); // 2 lines
+            Thread.Sleep(50);
+            int count = 0;
+            int row = 0;
+
+            foreach (char c in text)
+            {
+                if (c == '\n' || count == 16)
+                {
+                    count = 0;
+                    row++;
+                    if (row == 2) break;
+                    WriteCommandToDisplay(0xc0);
+                    if (c == '\n') continue;
+                }
+                count++;
+                _device.WriteByteRegister(0x40, (byte)c);
+            }
         }
-
-
     }
 }
-
--- StatisticalRobot-Lib/StatisticalRobot-LibTest/Program.cs
+++ StatisticalRobot-Lib/StatisticalRobot-LibTest/Program.cs
@@ -4,63 +4,71 @@ using Avans.StatisticalRobot;
 
 Robot.Wait(100);
 
-I2cDevice textDevice = Robot.CreateI2cDevice(0x3e);
+// I2cDevice textDevice = Robot.CreateI2cDevice(0x3e);
 
 int leftDistance;
 int rightDistance;
-Robot.SetDigitalPinMode(5,PinMode.Output);
+Robot.SetDigitalPinMode(5, PinMode.Output);
 Robot.SetDigitalPinMode(16, PinMode.Output);
 
-Robot.Motors(0,0);
+Robot.Motors(0, 0);
 
 ushort batteryData = Robot.ReadBatteryMillivolts();
 Console.WriteLine(batteryData);
 
-bool [] buttonData = Robot.ReadButtons();
+bool[] buttonData = Robot.ReadButtons();
 bool buttonA = buttonData[0];
 bool buttonB = buttonData[1];
 bool buttonC = buttonData[2];
 
-Robot.SetPwmPin(400,0.1);
+Robot.SetPwmPin(400, 0.1);
 Robot.StartPwm();
 
 Robot.Wait(2000);
 
 I2CDisplay display = new I2CDisplay(0x3e);
 display.SetText("Hello World");
+Robot.Wait(5000);
+while (true)
+{
+    display.SetText("Hello");
+    Robot.Wait(1000);
+    display.SetText("World");
+    Robot.Wait(1000);
+}
 
-
-while(!(buttonA && buttonB)) 
+while (!(buttonA && buttonB))
 {
     int[] humTempData = ReadDht11Data(18);
 
     //Console.WriteLine($"Humidity={humTempData[0]}.{humTempData[1]}%/nTemperature={humTempData[2]}.{humTempData[3]}C");
-    SetText($"Hum = {humTempData[0]}.{humTempData[1]} %\nTemp = {humTempData[2]}.{humTempData[3]} C");
+    // SetText($"Hum = {humTempData[0]}.{humTempData[1]} %\nTemp = {humTempData[2]}.{humTempData[3]} C");
 
     leftDistance = GetUltrasoneDistance(5);
     rightDistance = GetUltrasoneDistance(16);
 
-    if(leftDistance <= 20 || rightDistance <= 20)
+    if (leftDistance <= 20 || rightDistance <= 20)
     {
-        for(short i = 100; i > 0; i -= 5) {
+        for (short i = 100; i > 0; i -= 5)
+        {
             Robot.Motors(i, i);
             Robot.Wait(10);
         }
-        Robot.Motors(-120,120);
+        Robot.Motors(-120, 120);
         Robot.Wait(1000);
-        Robot.Motors(0,0);
+        Robot.Motors(0, 0);
     }
-    else if(leftDistance <= 30 && rightDistance >= 50)
+    else if (leftDistance <= 30 && rightDistance >= 50)
     {
-        Robot.Motors(100,60);
+        Robot.Motors(100, 60);
     }
-    else if(leftDistance >= 50 && rightDistance <= 30)
+    else if (leftDistance >= 50 && rightDistance <= 30)
     {
-        Robot.Motors(60,100);
+        Robot.Motors(60, 100);
     }
     else
     {
-        Robot.Motors(100,100);
+        Robot.Motors(100, 100);
     }
 
     buttonData = Robot.ReadButtons();
@@ -90,15 +98,15 @@ while(!(buttonA && buttonB))
 
     // if(buttonC) Robot.ChangePwmDutyCycle(0.5);
     // else        Robot.ChangePwmDutyCycle(0.0);
-    Robot.ChangePwmDutyCycle(0.5-(Robot.AnalogRead(0)/2000.0));
+    Robot.ChangePwmDutyCycle(0.5 - (Robot.AnalogRead(0) / 2000.0));
 
     //Console.WriteLine($"Sensor left distance ={GetUltrasoneDistance(5)} | Sensor right distance ={GetUltrasoneDistance(16)}");
 
     await Task.Delay(500);
 }
 
-Robot.Motors(0,0);
-Robot.WriteDigitalPin(5,PinValue.Low);
+Robot.Motors(0, 0);
+Robot.WriteDigitalPin(5, PinValue.Low);
 Robot.StopPwm();
 Robot.PlayNotes("o4l16ceg>c8");
 Console.WriteLine("The loop has ended");
@@ -117,7 +125,7 @@ static int GetUltrasoneDistance(int pin)
 
     Robot.SetDigitalPinMode(pin, PinMode.Input);
     int pulse = Robot.PulseIn(pin, PinValue.High, 50);
-    return pulse/29/2;
+    return pulse / 29 / 2;
 }
 
 static int[] ReadDht11Data(int DHTPIN)
@@ -175,61 +183,61 @@ static int[] ReadDht11Data(int DHTPIN)
     }
 }
 
-void TextCommand(byte cmd)
-    {
-        textDevice.WriteByteRegister(0x80, cmd);
-    }
+// void TextCommand(byte cmd)
+// {
+//     textDevice.WriteByteRegister(0x80, cmd);
+// }
 
-    // Set display text \n for second line (or auto wrap)
-void SetText(string text)
-{
-    TextCommand(0x01); // clear display
-    Thread.Sleep(50);
-    TextCommand(0x08 | 0x04); // display on, no cursor
-    TextCommand(0x28); // 2 lines
-    Thread.Sleep(50);
-    int count = 0;
-    int row = 0;
+// Set display text \n for second line (or auto wrap)
+// void SetText(string text)
+// {
+//     TextCommand(0x01); // clear display
+//     Thread.Sleep(50);
+//     TextCommand(0x08 | 0x04); // display on, no cursor
+//     TextCommand(0x28); // 2 lines
+//     Thread.Sleep(50);
+//     int count = 0;
+//     int row = 0;
 
-    foreach (char c in text)
-    {
-        if (c == '\n' || count == 16)
-        {
-            count = 0;
-            row++;
-            if (row == 2) break;
-            TextCommand(0xc0);
-            if (c == '\n') continue;
-        }
-        count++;
-        textDevice.WriteByteRegister(0x40, (byte)c);
-    }
-}
+//     foreach (char c in text)
+//     {
+//         if (c == '\n' || count == 16)
+//         {
+//             count = 0;
+//             row++;
+//             if (row == 2) break;
+//             TextCommand(0xc0);
+//             if (c == '\n') continue;
+//         }
+//         count++;
+//         textDevice.WriteByteRegister(0x40, (byte)c);
+//     }
+// }
 
-    // Update the display without erasing the display
-void SetTextNoRefresh(string text)
-{
-    TextCommand(0x02); // return home
-    Thread.Sleep(50);
-    TextCommand(0x08 | 0x04); // display on, no cursor
-    TextCommand(0x28); // 2 lines
-    Thread.Sleep(50);
-    int count = 0;
-    int row = 0;
+// Update the display without erasing the display
+// void SetTextNoRefresh(string text)
+// {
+//     TextCommand(0x02); // return home
+//     Thread.Sleep(50);
+//     TextCommand(0x08 | 0x04); // display on, no cursor
+//     TextCommand(0x28); // 2 lines
+//     Thread.Sleep(50);
+//     int count = 0;
+//     int row = 0;
 
-    text = text.PadRight(32);
+//     text = text.PadRight(32);
 
-    foreach (char c in text)
-    {
-        if (c == '\n' || count == 16)
-        {
-            count = 0;
-            row++;
-            if (row == 2) break;
-            TextCommand(0xc0);
-            if (c == '\n') continue;
-        }
-        count++;
-        textDevice.WriteByteRegister(0x40, (byte)c);
-    }
-}
+//     foreach (char c in text)
+//     {
+//         if (c == '\n' || count == 16)
+//         {
+//             count = 0;
+//             row++;
+//             if (row == 2) break;
+//             TextCommand(0xc0);
+//             if (c == '\n') continue;
+//         }
+//         count++;
+//         textDevice.WriteByteRegister(0x40, (byte)c);
+//     }
+// }
